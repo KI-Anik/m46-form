fa7c39f0e8758ac79e744dfde5f9da4ef741b61e
KI-Anik <iamkhairul101@gmail.com>
2024-11-28 18:54:56 +0600

    form add and input value display on clg

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import SimpleForm from './components/Form/SimpleForm';
+
+
+const App = () => {
+  return (
+    <div>
+      <SimpleForm></SimpleForm>
+    </div>
+  );
+};
+
+export default App;
