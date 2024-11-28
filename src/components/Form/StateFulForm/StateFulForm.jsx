f9c707a5e3e86dae547b9f8e3aed26b33191ab64
KI-Anik <iamkhairul101@gmail.com>
2024-11-28 20:43:09 +0600

    password conditional error applied

--- src/components/Form/StateFulForm/StateFulForm.jsx
+++ src/components/Form/StateFulForm/StateFulForm.jsx
@@ -2,42 +2,54 @@ import { useState } from "react";
 
 const StateFulForm = () => {
     const [name, setName] = useState(null)
-    const [email, setEmail ] = useState(null)
+    const [email, setEmail] = useState(null)
     const [pass, setPass] = useState(null)
-
+    const [error, setError] = useState('')
+ 
     const handleSubmit = e => {
         e.preventDefault()
-        console.log('name : ', name, '/ email :', email, '/ password:', pass, )
+
+        if(pass.length < 6){
+            setError('passeword must be 6 character')
+        }
+        else{
+            console.log('name : ', name, '/ email :', email, '/ password:', pass,)
+            setError('')
+        }
     }
 
     const handleNameChange = e => {
         setName(e.target.value)
     }
 
-const handleEmailChange = e => {
-    setEmail(e.target.value)
-}
+    const handleEmailChange = e => {
+        setEmail(e.target.value)
+    }
 
-const handlePassChange = e => {
-    setPass(e.target.value)
-}
+    const handlePassChange = e => {
+        setPass(e.target.value)
+    }
 
     return (
         <div>
-             <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <input
-                onChange={handleNameChange}
-                type="text" name='name' />
+                    onChange={handleNameChange}
+                    type="text" name='name' />
                 <br />
                 <input
-                onChange={handleEmailChange}
-                 type="email" name='email' />
+                    onChange={handleEmailChange}
+                    type="email" name='email' />
                 <br />
                 <input
-                onChange={handlePassChange}
-                type="password" name='password' />
+                    onChange={handlePassChange}
+                    type="password" name='password' required />
                 <br />
                 <input type="submit" />
+                {
+                    error &&  <p>{error}</p>
+                }
+               
             </form>
         </div>
     );
