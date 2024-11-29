521dc893591d7845839f47c33217ca26d56af646
KI-Anik <iamkhairul101@gmail.com>
2024-11-29 11:08:13 +0600

    reUsable component applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import './App.css'
-import HookForm from './components/HookForm/HookForm';
+// import HookForm from './components/HookForm/HookForm';
+import ReUsableForm from './components/ReUsableForm/ReUsableForm';
 
 
 const App = () => {
+
+  const handleSignUp = data => {
+    console.log('sign up', data)
+}
+
+const handleUpdateProfile = data => {
+  console.log('update profile ', data)
+}
+
   return (
     <div>
       {/* <SimpleForm></SimpleForm> */}
       {/* <StateFulForm></StateFulForm> */}
       {/* <RefForm></RefForm> */}
+      {/* <HookForm></HookForm> */}
+      <ReUsableForm formTitle={'sign Up'}
+       handleSubmit={handleSignUp}>
+       </ReUsableForm>
 
-      <HookForm></HookForm>
+      <ReUsableForm formTitle={'Profile Update'}
+       handleSubmit={handleUpdateProfile} 
+       submitBtnText='Update'></ReUsableForm>
+
     </div>
   );
 };
